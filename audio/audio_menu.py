846fb53e110460dc6d444ddde5b8ffa5f4787303
Mayur8805 <mayurmakwana123.0@gmail.com>
2026-05-10 16:07:46 +0530

    Added YouTube Music Scraper

--- audio/audio_menu.py
+++ audio/audio_menu.py
@@ -1,8 +1,12 @@
 from core.menu import select_option
 from audio.youtube import youtube_audio_scraper
+from audio.youtube_music import youtube_music_scraper
 
 def audio_menu():
-    selected = select_option(["YouTube", "Spotify (Coming Soon)"], True)
+    selected = select_option(["YouTube","YouTube Music", "Spotify (Coming Soon)"], True)
 
     if selected == 0:
         youtube_audio_scraper()
+
+    if selected == 1:
+        youtube_music_scraper()
